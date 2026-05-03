4bb18d8894cf3d5cc733831d73b143883bb33be7
Samy Ghebache <121521238+samy-ghebache@users.noreply.github.com>
2026-05-03 19:38:02 +0200

    Add: permutation-string - submission-14

---
+++ Data Structures & Algorithms/permutation-string/submission-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1HashMap = Counter(s1)
+        s2HashMap = Counter(s2[:len(s1)])
+
+        if s2HashMap == s1HashMap:
+            return True
+        
+        for i in range(len(s1), len(s2)):
+            s2HashMap[s2[i]] += 1
+            s2HashMap[s2[i - len(s1)]] -= 1
+            if s2HashMap[s2[i - len(s1)]] == 0:
+                del s2HashMap[s2[i - len(s1)]]
+
+            if s2HashMap == s1HashMap:
+                return True
+
+        return False
